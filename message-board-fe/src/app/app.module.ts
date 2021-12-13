96836b310fd253df871cd38123a03312e44607cd
Esraa_Kamel <esraa.kamel1811@gmail.com>
2021-12-13 18:48:56 +0200

    Validations for registeration form

--- message-board-fe/src/app/app.module.ts
+++ message-board-fe/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
@@ -35,6 +35,7 @@ import { RegisterationComponent } from './registeration/registeration.component'
     BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     MatButtonModule,
     MatCardModule,
     MatInputModule,
--- message-board-fe/src/app/registeration/registeration.component.ts
+++ message-board-fe/src/app/registeration/registeration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registeration',
@@ -7,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class RegisterationComponent implements OnInit {
-
-  constructor() { }
+  public registerForm: FormGroup = new FormGroup({});
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required ],
+      lastName: ['', Validators.required ],
+      email: ['', Validators.required ],
+      password: ['', Validators.required ],
+      confirmPassword: ['', Validators.required ]
+    })
   }
 
+  onSubmit() {
+    console.log( this.registerForm.value );
+  }
+
+  isValid(control: string) {
+    return this.registerForm.controls[control].invalid && this.registerForm.controls[control].touched;
+  }
 }
