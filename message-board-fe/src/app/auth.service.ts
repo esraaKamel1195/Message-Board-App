2e01c9807299897af9033f946ecddf0128fa1d28
Esraa_Kamel <esraa.kamel1811@gmail.com>
2021-12-14 18:13:39 +0200

    implement authentication check

--- message-board-fe/src/app/auth.service.ts
+++ message-board-fe/src/app/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -8,13 +9,29 @@ export class AuthService {
 
   BASE_URL: string = 'http://localhost:3000/auth';
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private router: Router
   ) { }
+
+  get name() {
+    return localStorage.getItem('name');
+  }
+
+  get isAuthenticated() {
+    return !!localStorage.getItem('token');
+  }
 
   register( user: any ) {
     delete user.confirmPassword;
-    this.http.post<string>(this.BASE_URL + '/register', user).subscribe( (res)=> {
-      localStorage.setItem('token', res );
+    this.http.post<{ firstName: string, token: string }>(this.BASE_URL + '/register', user).subscribe( (res)=> {
+      let authResponse = res;
+      if(!authResponse.token) {
+        return;
+      }
+      localStorage.setItem( 'token', authResponse.token );
+      localStorage.setItem( 'name', authResponse.firstName );
+
+      this.router.navigate(['/'])
     });
   }
 }
