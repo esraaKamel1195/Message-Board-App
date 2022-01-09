3652c75cb6e498f9d8fbb98979d700f57adfd258
Esraa_Kamel <esraa.kamel1811@gmail.com>
2022-01-10 00:37:51 +0200

    Message & user schema Models

---
+++ message-board-be/models/userModel.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const Schema = mongoose.Schema;
+
+export const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    hashPassword: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+UserSchema.methods.comparePassword = function (password, hashPassword) {
+    return bcrypt.compareSync( password, hashPassword );
+};
